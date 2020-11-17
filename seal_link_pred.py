d9152e969d4b754721ac152aa8d4d2b4f6a9ac0a
muhanzhang <muhan.zhang@hotmail.com>
2020-11-16 19:30:07 -0800

    add GCN

--- seal_link_pred.py
+++ seal_link_pred.py
@@ -210,6 +210,67 @@ class SEALDynamicDataset(Dataset):
         return data
 
 
+class GCN(torch.nn.Module):
+    def __init__(self, hidden_channels, num_layers, max_z, 
+                 use_feature=False, node_embedding=None, 
+                 dropout=0.5):
+        super(SAGE, self).__init__()
+        self.use_feature = use_feature
+        self.node_embedding = node_embedding
+        self.max_z = max_z
+        self.z_embedding = Embedding(self.max_z, hidden_channels)
+
+        self.convs = torch.nn.ModuleList()
+        initial_channels = hidden_channels
+        if self.use_feature:
+            initial_channels += dataset.num_features
+        if self.node_embedding is not None:
+            initial_channels += node_embedding.embedding_dim
+        self.convs.append(GCNConv(initial_channels, hidden_channels, cached=True))
+        for _ in range(num_layers - 1):
+            self.convs.append(SAGEConv(hidden_channels, hidden_channels, cached=True))
+
+        self.dropout = dropout
+        self.lin1 = Linear(hidden_channels, hidden_channels)
+        self.lin2 = Linear(hidden_channels, 1)
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, z, edge_index, batch, x=None, edge_weight=None, node_id=None):
+        z_emb = self.z_embedding(z)
+        if z_emb.ndim == 3:  # in case z has multiple integer labels
+            z_emb = z_emb.sum(dim=1)
+        if self.use_feature and x is not None:
+            x = torch.cat([z_emb, x.to(torch.float)], 1)
+        else:
+            x = z_emb
+        if self.node_embedding is not None and node_id is not None:
+            n_emb = self.node_embedding(node_id)
+            x = torch.cat([x, n_emb], 1)
+        for conv in self.convs[:-1]:
+            x = conv(x, edge_index, edge_weight)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, edge_index)
+        if True:  # center pooling
+            _, center_indices = np.unique(batch.cpu().numpy(), return_index=True)
+            x_src = x[center_indices]
+            x_dst = x[center_indices + 1]
+            x = (x_src * x_dst)
+            x = F.relu(self.lin1(x))
+            x = F.dropout(x, p=self.dropout, training=self.training)
+            x = self.lin2(x)
+        else:  # sum pooling
+            x = global_add_pool(x, batch)
+            x = F.relu(self.lin1(x))
+            x = F.dropout(x, p=self.dropout, training=self.training)
+            x = self.lin2(x)
+
+        return x
+
+
 class SAGE(torch.nn.Module):
     def __init__(self, hidden_channels, num_layers, max_z, 
                  use_feature=False, node_embedding=None, 
