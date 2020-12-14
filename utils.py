8566f5d4b09dc4467428889c58e73af7c6e59469
muhanzhang <muhan.zhang@hotmail.com>
2020-12-14 11:13:06 -0800

    better wording

--- utils.py
+++ utils.py
@@ -299,7 +299,7 @@ class Logger(object):
             argmax = result[:, 0].argmax().item()
             print(f'Run {run + 1:02d}:', file=f)
             print(f'Highest Valid: {result[:, 0].max():.2f}', file=f)
-            print(f'Highest Epoch: {argmax + 1}', file=f)
+            print(f'Highest Eval Point: {argmax + 1}', file=f)
             print(f'   Final Test: {result[argmax, 1]:.2f}', file=f)
         else:
             result = 100 * torch.tensor(self.results)
